0c1463e87f8e4bbb67f995aa27a5c0746c8306e9
iammrmikeman <iammrmikeman25@gmail.com>
2025-06-15 09:15:46 +0100

    0.8.8

--- EXE/LATEST/main.js
+++ EXE/LATEST/main.js
@@ -1,31 +1,17 @@
-
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
     frame: false,
-    resizable: true,
-    fullscreenable: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
-      contextIsolation: false,
+      contextIsolation: false
     }
   });
-
-  win.loadFile('index.html');
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
-
-app.whenReady().then(() => {
-  createWindow();
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+app.whenReady().then(() => { createWindow(); });
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
