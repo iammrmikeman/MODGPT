f4dd3af80fffeaa04e66e54fef70a91f75f202c3
iammrmikeman <iammrmikeman25@gmail.com>
2025-06-15 05:49:37 +0100

    0.6.0

--- EXE/BUILDS/ACTIVE/script.js
+++ EXE/BUILDS/ACTIVE/script.js
@@ -2,7 +2,6 @@
 const dashboard = document.getElementById('dashboard');
 const submenu = document.getElementById('submenu');
 
-// Folder label to icon file map
 const iconMap = {
   'EMAILS': 'emails_folder.png',
   'COMPANY': 'projects_folder.png',
@@ -10,17 +9,16 @@ const iconMap = {
   'MEMORY LOGS': 'memory_folder.png',
   'TRUSTED BUILDS': 'trusted_builds_folder.png',
   'THEMES': 'xmb_themes_folder.png',
-  'MODGPT': 'icon_128_MODGPT_PERFECT.png'
+  'MODGPT': 'modgpt_folder.png'
 };
 
 function renderTopLevelFolders() {
   dashboard.innerHTML = '';
   Object.keys(window.folderMap).forEach(folder => {
     const div = document.createElement('div');
-    const icon = iconMap[folder] || 'folder.png';
     div.className = 'icon';
-    div.innerHTML = `<img src="${icon}"><div class="label">${folder}</div>`;
-    div.onclick = () => renderSubmenu(folder);
+    div.innerHTML = `<img src="${iconMap[folder]}"><div class="label">${folder}</div>`;
+    div.addEventListener('click', () => renderSubmenu(folder));
     dashboard.appendChild(div);
   });
 }
@@ -31,7 +29,6 @@ function renderSubmenu(folder) {
     const div = document.createElement('div');
     div.className = 'subitem';
     div.innerText = item;
-    div.onclick = () => alert(`Open: ${folder} → ${item}`);
     submenu.appendChild(div);
   });
 }
